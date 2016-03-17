660f2539d1ef05b7afbfa886f77cb6b0367d3d29
blikethat <kamie.lyn@gmail.com>
2016-03-16 23:52:42 -0700

    implemented post route for Slack

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
 var express = require('express');
-
+var http = require("request");
+var bodyParser = require('body-parser');
+var secretSlack = require("./secrets/slack.js");
 var app = express();
+
+app.use(bodyParser.json());
 
 app.get("/", function(request, response) {
   response.send('Vegeta, what does the scouter say???');
 });
+
+app.post("/api/slack/signup", function(request, response) {
+  console.log(request.body);
+  request.post(
+    {
+      url: 'https://la-coders.slack.com/api/users.admin.invite',
+      form: {
+        email: request.body.email,
+        token: secretSlack.token,
+        set_active: true
+      }
+    },function(err, response) {
+        if(err) {
+          console.log(err);
+        } else {
+          console.log(response, ' it was a success');
+        }
+      }
+  );
+
+});
+
 
 app.listen(9001, function() {
   console.log("App listening on port 9001");
